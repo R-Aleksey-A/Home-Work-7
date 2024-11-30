fbc5d5d805b25001743fbf1ea08f1218a4c67fba
huawei <romanec.aleksey@gmail.com>
2024-11-30 17:02:56 +0400

    HW7.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        {System.out.println("Home Work-7");}
+
+        System.out.println("Home Work-7");
+
+
+        System.out.println("Задание-1");
+
+        int moneyMonthly = 15000;
+        int accumulation = 2_459_000;
+        int result = 0;
+        int month = 0;
+        while (result < accumulation) {
+            moneyMonthly = moneyMonthly + moneyMonthly / 100;
+            result = result + moneyMonthly;
+            month++;
+
+        }
+        System.out.println("Месяц " + month + " - сумма накоплений равна " + result + " рублей");
     }
 }
